06fcbe70acfe056f89366e7ba7d8666dc1db6137
nao-fujimura-09 <awakomik@gmail.com>
2021-03-01 12:43:43 +0900

    migration&seeder fix2

--- database/migrations/2021_02_25_222113_create_posts_table.php
+++ database/migrations/2021_02_25_222113_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string ('title', 20);
+            $table->string('title', 20);
             $table->text('body');
             $table->timestamps();
         });
