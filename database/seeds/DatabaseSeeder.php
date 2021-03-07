3f2074d4b61e524b16a453eacbcec836a8a16974
Soya0719 <kirby0719@gmail.com>
2021-03-07 21:55:52 +0000

    データーベースシーダーの変更をしました。

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,29 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'title' => 'sample1',
-            'body' => 'aaa', 
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => 'sample2',
-            'body' => 'bbb', 
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => 'sample3',
-            'body' => 'ccc', 
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => 'sample4',
-            'body' => 'ddd', 
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => 'sample5',
-            'body' => 'eee', 
-        ]);
+        $this->call(PostsTableSeeder::class);
     }
 }
