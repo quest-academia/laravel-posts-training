5129c06bcebd6e4ce5d368b0f92b84175ebd4adf
ryuhei-yoshi <dogennmosenn@gmail.com>
2021-03-08 08:18:51 +0000

    create: 投稿一覧画面

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-dark">
+    <div class="container text-right text-light">
+        <p>@Kyohei-Yamamoto All right reserved</p>
+    </div>
+</footer>
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,5 @@
+<header class="bg-light">
+    <div class="container">
+        <h1>Hacksレビュー</h1>
+    </div>
+</header>
