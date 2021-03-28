9de10b369e0ab40d38dac881b26e91a08d5c7249
otani <daguru0409@gmail.com>
2021-03-28 16:58:15 +0900

    投稿一覧画面

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class = "container">
+        <div class="container my-4">
+            <a class="btn btn-primary" href="">投稿の新規作成</a>
+        </div>
+    </div>
+
+    <div class="container my-4">
+
+     @foreach($posts as $post)
+        <div class="card mb-4">
+            <div class="card-header">
+                {{ $post->title }}
+            </div>
+
+         <div class="card-body">
+            <p class="card-text">
+               {{ $post->body }}
+            </p>
+                <a class="card-link" href="posts.show">詳細をみる</a>
+         </div>
+            <div class="card-footer">
+                投稿日時:{{ $post->created_at->format('Y.m.d') }}
+            </div>
+        </div>
+    @endforeach
+@endsection
