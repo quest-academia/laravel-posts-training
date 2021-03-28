b1636fb5d531528c08ddb25479fba05211a82873
nao-fujimura-09 <awakomik@gmail.com>
2021-03-29 01:58:34 +0900

    PostsTableSeederの修正

--- database/migrations/2021_03_26_221846_create_posts_table.php
+++ database/migrations/2021_03_26_221846_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
             $table->string('name');
             $table->string('title', 50);
             $table->text('body');
