92ec2c7a984641b2db1343a9a8e30e16299caa2b
Akrjourney <akarikuma1993@gmail.com>
2021-03-29 04:03:03 +0000

    外部キー追加

--- database/migrations/2021_01_13_060830_create_posts_table.php
+++ database/migrations/2021_01_13_060830_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('title', 50);
             $table->text('body');
             $table->timestamps();
