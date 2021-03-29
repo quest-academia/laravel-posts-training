36b0b15a546361d6fa8d5d900d6f6ac98ea19970
nao-fujimura-09 <awakomik@gmail.com>
2021-03-29 16:57:02 +0900

    PostsTable_外部キーと一対多

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }
