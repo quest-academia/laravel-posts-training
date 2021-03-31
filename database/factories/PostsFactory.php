20b4d602a820cdb501bd278a7a793fa10ce11bc6
nao-fujimura-09 <awakomik@gmail.com>
2021-04-01 03:46:00 +0900

    Posts_Table_Seeder_Foreign_Fix

--- database/factories/PostsFactory.php
+++ database/factories/PostsFactory.php
@@ -3,12 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Post;
+use App\User;
 use Faker\Generator as Faker;
 
-$factory->define(App\Post::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker)
+{
     return [
-        'name' => $faker->name,
-        'title' => $faker->realText(50),
+        'user_id' => factory(User::class)->create()->id,
+        'title' => $faker->realText(10),
         'body' => $faker->realText(100),
     ];
 });
--- database/migrations/2021_03_26_221846_create_posts_table.php
+++ database/migrations/2021_03_26_221846_create_posts_table.php
@@ -15,11 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title', 50);
             $table->text('body');
             $table->timestamps();
-            $table->foreign('id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
