646f7c8e8b89dda9d2dee0c8dc93e910bd63a0eb
nao-fujimura-09 <awakomik@gmail.com>
2021-04-02 15:58:54 +0900

    Comments_Table_Seeder_Fix

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,7 +10,7 @@ class PostsController extends Controller
 
     public function index()
     {
-        //$posts = Post::orderBy('created_at', 'desc')->paginate(10);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
     }
 
 }
