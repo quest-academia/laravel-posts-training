5dbc79133a399c9a63d2a7df80f8204c7a8cdb5f
otani <daguru0409@gmail.com>
2021-04-04 13:53:12 +0900

    ユーザ詳細画面

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 // ログイン画面
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
+
+//ユーザー詳細画面
+Route::get('users/{id}','UsersController@show');
