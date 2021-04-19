1e9c787ba0f3a392f67699812fda679a79fd63b2
Soya0719 <kirby0719@gmail.com>
2021-04-19 14:15:38 +0000

    edit.blade.phpの編集

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,14 +7,13 @@
         <div class="border p-4">
             <h5 class="mb-4">投稿の編集</h5>
 @include('commons.error_clear_last')
-            {!!Form::open(['route'=>'posts.store'])!!}
-                <div class="form-group">
-                {!!Form::label('title','タイトル')!!}
-                {!!Form::text('title',old('title'),['class'=>'form-control'])!!}
+            {!!Form::open(['route'=>['posts.update',$post->id]])!!}
+            {{ csrf_field() }}
+                <div class="md-form">
+                {!! Form::text('title', old('title'), ['class' => 'form-control'.(old('title') ? old('title'):$post->title)]) !!}
                 </div>
                 <div class="form-group">
-                {!!Form::label('body','本文')!!}
-                {!!Form::textarea('body',old('body'),['class'=>'form-control'])!!}
+                {!! Form::textarea('body', old('body'),['class'=> 'form-control'.(old('body') ? old('body'):$post->body)]) !!}
                 </div>
                 <div class="mt-4">
                     <a class="btn btn-secondary"href="">キャンセル</a>
