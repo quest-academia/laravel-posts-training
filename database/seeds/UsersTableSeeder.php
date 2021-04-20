aa34d3358e7ddbf7c4f78c64c9d7930885ae3e67
Soya0719 <kirby0719@gmail.com>
2021-04-20 16:01:19 +0000

    config/app.blade,databaseのシーダー、ユーザーズテーブルの作成

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,40 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => "テスト１",
+            'email' => "test1@example.com",
+            'password' => bcrypt('secret'),
+            'name' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('users')->insert([
+            'name' => "テスト2",
+            'email' => "test2@example.com",
+            'password' => bcrypt('secret'),
+            'name' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('users')->insert([
+            'name' => "テスト3",
+            'email' => "test3@example.com",
+            'password' => bcrypt('secret'),
+            'name' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('users')->insert([
+            'name' => "テスト4",
+            'email' => "test4@example.com",
+            'password' => bcrypt('secret'),
+            'name' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('users')->insert([
+            'name' => "テスト5",
+            'email' => "test5@example.com",
+            'password' => bcrypt('secret'),
+            'name' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
     }
 }
