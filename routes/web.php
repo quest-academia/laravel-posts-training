efaef4768a33f7c97b72ea12dcd1d38951f4ea71
Soya0719 <kirby0719@gmail.com>
2021-04-22 15:08:20 +0000

    develop_3rd確認のため

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
