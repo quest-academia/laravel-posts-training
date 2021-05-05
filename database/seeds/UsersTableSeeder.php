3fdc104acdce23035855342fbb579bdaf48786a0
Kou <hikkappi@yahoo.co.jp>
2021-05-05 13:00:25 +0900

    fix: UsersTableSeederにて複数のデータを登録するように修正。

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -12,12 +11,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10)."@example.com",
-            'password' => bcrypt('password'),
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
-        ]);
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => 'user'.(string)$i,
+                'email' => 'user'.(string)$i."@example.com",
+                'password' => bcrypt('password'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
     }
 }
