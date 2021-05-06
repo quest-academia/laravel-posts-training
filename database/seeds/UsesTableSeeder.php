07a7e66895d52aad7bc0e7496eac456ad5150c53
EC2 Default User <ec2-user@ip-172-31-3-23.us-east-2.compute.internal>
2021-05-06 05:58:54 +0000

    add:シーディングの内容追加

--- database/seeds/UsesTableSeeder.php
+++ database/seeds/UsesTableSeeder.php
@@ -11,6 +11,30 @@ class UsesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'sample1',
+            'email' => 'sample1@sample.com',
+            'password' => bcrypt('sample1') 
+        ]);
+        DB::table('users')->insert([
+            'name' => 'sample2',
+            'email' => 'sample2@sample.com',
+            'password' => bcrypt('sample2') 
+        ]);
+        DB::table('users')->insert([
+            'name' => 'sample3',
+            'email' => 'sample3@sample.com',
+            'password' => bcrypt('sample3') 
+        ]);
+        DB::table('users')->insert([
+            'name' => 'sample4',
+            'email' => 'sample4@sample.com',
+            'password' => bcrypt('sample4') 
+        ]);
+        DB::table('users')->insert([
+            'name' => 'sample5',
+            'email' => 'sample5@sample.com',
+            'password' => bcrypt('sample5') 
+        ]);
     }
 }
