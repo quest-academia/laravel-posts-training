6b39b8825dc1fedccec35590e85b12404cf3e3da
masaki420 <zyab5275@gmail.com>
2021-05-06 17:35:58 +0900

    フッター・ヘッダー

---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+			<nav class="navbar navbar-expand navbar-light bg-light">
+				<a href="/" class="navbar-brand">
+					<i class="fas fa-home fa-2x"></i>
+				</a>
+				<div class="collapse navbar-collapse">
+					<ul class="navbar-nav ml-md-auto align-items-center">
+						<li><a href="" class="btn btn-info">
+							<i class="fas fa-pencil-alt"></i>
+							投稿する
+						</a></li>
+						<li>
+							<a href="" class="nav-link far fa-user fa-lg"></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div class="jumbotron">
+				<h1 class="display-4 text-center">Laravel Post
+					<i class="fas fa-mail-bulk"></i>
+				</h1>
+			</div>
+	</header>
