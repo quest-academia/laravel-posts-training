c7f96c4a950aa0bfbceef120f87e1f0a8c87fc97
masaki420 <zyab5275@gmail.com>
2021-05-08 19:21:39 +0900

    jumbrotronの削除

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -15,10 +15,4 @@
 					</ul>
 				</div>
 			</nav>
-			<div class="jumbotron">
-				<h1 class="display-4 text-center">Laravel Post
-					<i class="fas fa-mail-bulk"></i>
-				</h1>
-			</div>
 	</header>
-	
