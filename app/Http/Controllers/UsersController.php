eb4590a4f4d8b96ae8b62534d8f024fb55d1d8cd
Kou <hikkappi@yahoo.co.jp>
2021-05-09 15:55:57 +0900

    add: ユーザー更新画面実装。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserUpdateRequest;
 use Illuminate\Http\Request;
 use App\User;
 
@@ -16,5 +17,36 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', ['user' => $user]);
+    }
+
+    /**
+     * ユーザー更新画面
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        return view('users.edit');
+    }
+
+    /**
+     * ユーザー更新処理
+     *
+     * @param App\Http\Requests\UserUpdateRequest $request
+     * @param App\User $user
+     */
+    public function update(UserUpdateRequest $request, User $user)
+    {
+        $user = User::find($request->id);
+
+        if ($request->email == $user->email) {
+            // メールアドレスの変更なし
+            $user->fill($request->except('email'))->save();
+        } else {
+            // メールアドレスの変更あり
+            $user->fill($request->all())->save();
+        }
+
+        return redirect()->route('users.show', ['user' => $user]);
     }
 }
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="col-md-offset-2 mb-1 edit-profile-wrapper my-5 ">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                @include('layouts.error_card_list')
+                <div class="profile-form-wrap">
+                    <form class="edit_user" enctype="multipart/form-data" action="{{ route('users.update') }}" accept-charset="UTF-8" method="post">
+                        <input name="utf8" type="hidden" value="&#x2713;" />
+                        <input type="hidden" name="id" value="{{ Auth::id() }}" />
+                        {{ csrf_field() }}
+                        @method('put')
+                        <div class="form-group">
+                            <label for="name">ユーザー名</label>
+                            <input autofocus="autofocus" class="form-control" value="{{ old('name') ?? Auth::user()->name }}" name="name" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">メールアドレス</label>
+                            <input autofocus="autofocus" class="form-control" value="{{ old('email') ?? Auth::user()->email }}" name="email" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">パスワード</label>
+                            <input autofocus="autofocus" class="form-control" type="password" name="password" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">パスワードの確認</label>
+                            <input autofocus="autofocus" class="form-control" type="password" name="password_confirmation" />
+                        </div>
+
+                        <div class="text-center">
+                            <input type="submit" name="commit" value="変更する" class="btn btn-primary" data-disable-with="変更する" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
