2950605ef02f2d8e35346e08781e24e8ecaab360
岩山和哉 <iwayamakazuya@iwayamakazuyanoMacBook-Air.local>
2021-05-10 23:25:00 +0900

    空行追加&PostsController変更

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,8 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
-        
+        $posts = Post::orderBy('created_at', 'desc')->get();
+
         return view('post.index', ['posts' => $posts]);
     }
 }
