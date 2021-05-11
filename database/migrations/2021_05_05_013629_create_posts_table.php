d0ba6c17ce52ca8cb1882f1552a001e3de1b43a5
Kou <hikkappi@yahoo.co.jp>
2021-05-11 23:11:29 +0900

    fix: postsマイグレーションファイルのidをbigIntegerに修正。

--- database/migrations/2021_05_05_013629_create_posts_table.php
+++ database/migrations/2021_05_05_013629_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned()->index();
             $table->string('title', 50);
             $table->text('message');
