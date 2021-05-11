1decd32caad90ecc926bde0ecd3c9d0b556d606d
岩山和哉 <iwayamakazuya@iwayamakazuyanoMacBook-Air.local>
2021-05-11 10:05:41 +0900

    新投稿登録機能作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
@@ -17,5 +18,33 @@ class PostsController extends Controller
         $posts = Post::orderBy('created_at', 'desc')->get();
 
         return view('post.index', ['posts' => $posts]);
+    }
+
+    /**
+     * ユーザー新規登録
+     *
+     * @return $user
+     */
+    public function create()
+    {
+        $user = Auth::user();
+
+        return view('post.create', ['user' => $user]);
+    }
+
+    /**
+     * ユーザー登録保存
+     *
+     * @return $post　登録内容
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->user_id = auth()->user()->id;
+        $post->title=$request->title;
+        $post->message=$request->message;
+        $post->save();
+        
+        return redirect('/');
     }
 }
--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand navbar-light ">
-    <a class="navbar-brand" href=""><i class="fas fa-home fa-2x"></i>
+    <a class="navbar-brand" href="/"><i class="fas fa-home fa-2x"></i>
     </a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-md-auto align-items-center">
             <li>
-                <a class="btn btn-info btn-lg " href=""><i class="fas fa-pencil-alt"></i>
-                投稿する
+                <a class="btn btn-info btn-lg " href="{{route('posts.create')}}"><i class="fas fa-pencil-alt"></i>
+                    投稿する
                 </a>
             </li>
             <li>
