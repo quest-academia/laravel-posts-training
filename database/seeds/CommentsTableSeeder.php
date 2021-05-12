b94494fc0fe04b314e97f3179e8786bff7de934c
Kou <hikkappi@yahoo.co.jp>
2021-05-12 19:55:16 +0900

    fix: 複数のダミーデータを挿入するように修正。

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -11,28 +11,30 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('comments')->insert([
-            [
-                'user_id' => rand(0, 9),
-                'post_id' => 1,
-                'comment' => "コメント1",
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'user_id' => rand(0, 9),
-                'post_id' => 2,
-                'comment' => "コメント2",
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'user_id' => rand(0, 9),
-                'post_id' => 3,
-                'comment' => "コメント3",
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-        ]);
+        for ($i = 1; $i <= 5; $i++) {
+            DB::table('comments')->insert([
+                [
+                    'user_id' => rand(1, 10),
+                    'post_id' => rand(1, 3),
+                    'comment' => "コメント{$i}-1",
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'user_id' => rand(1, 10),
+                    'post_id' => rand(1, 3),
+                    'comment' => "コメント{$i}-2",
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'user_id' => rand(1, 10),
+                    'post_id' => rand(1, 3),
+                    'comment' => "コメント{$i}-3",
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+            ]);
+        }
     }
 }
