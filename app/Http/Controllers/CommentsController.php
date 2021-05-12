55ad688b5c8f0c982eea4f6cbea74ffc832d26ab
Masaki A <83165276+masaki420@users.noreply.github.com>
2021-05-13 06:16:05 +0900

    Update CommentsController.php

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+    public function store(Request $request, $postId) 
+    {
+        $this->validate($request, [
+        'body' => 'required'
+        ]);
+
+        $comment = new Comment(['body' => $request->body]);
+        $post = Post::findOrFail($postId);
+        $post->comments()->save($comment);
+
+        return redirect()
+        ->action('PostsController@show', $post->id);
+    }
 }
