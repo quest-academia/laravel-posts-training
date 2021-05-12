25e56ec55b1dc6d53ce097cee28b047566c1d59a
masaki420 <zyab5275@gmail.com>
2021-05-12 15:29:46 +0000

    Index.blade.php追加

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['user_id','title','body'];
+    
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    
+    public function comments() {
+    return $this->hasMany('App\Comment');
+    }
 }
