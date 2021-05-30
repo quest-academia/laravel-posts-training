4cbfdcc25a6591ba6667b03280441287b1fa4d7f
masaki420 <zyab5275@gmail.com>
2021-05-30 17:36:26 +0000

    CommentsController編集

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,25 +8,6 @@ use App\Comment;
 
 class CommentsController extends Controller
 {
-    public function index()
-    {
-        $comments = Comment::orderBy('created_at','asc')->get();
-
-        return view('posts.index',['comments'=>$comments]);
-    }
     
-    public function store(CommentRequest $request)
-    {
-        $savedata = [
-            'user_id' => $request->user_id,
-            'post_id' => $request->post_id,
-            'body' => $request->body,
-        ];
- 
-        $comment = new Comment;
-        $comment->fill($savedata)->save();
- 
-        return redirect()->route('/', [$savedata['post_id']]);
-    }
 }
 
