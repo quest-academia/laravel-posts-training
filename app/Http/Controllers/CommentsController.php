e8752c27fe063313d47084adcd21ce4539af6ebe
EC2 Default User <ec2-user@ip-172-31-3-23.us-east-2.compute.internal>
2021-06-07 13:49:38 +0000

    add:CommentsControllerにuseの追加

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\CommentRequest;
 use App\Comment;
+use Illuminate\Support\Facades\Auth;
+
 
 class CommentsController extends Controller
 {
