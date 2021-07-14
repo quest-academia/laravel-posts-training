b8289e0a9c29ca9c5bb7297da02adaf2a425f7a3
後藤和英 <gotogo2927@gmail.com>
2021-07-14 11:05:34 +0900

    comments（コメント）テーブルのマイグレーションとシーダー作成 修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // PostsTableSeeder::class,
+            PostsTableSeeder::class,
             CommentsTableSeeder::class
         ]);
     }
